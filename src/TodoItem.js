4908d7b4161e98ec236a42ad59ae7c89346b7340
samz_chou <samz_chou@163.com>
2019-06-24 17:46:18 +0800

    updates

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class TodoItem extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
 	handleDelete() {
+		//const { delete, index } = this.props;
 		this.props.delete(this.props.index);
 	}
 	render() {
+		const { content } = this.props;
 		return (
-			<div onClick={this.handleDelete.bind(this)}>
-				{this.props.content}{' '}
+			<div onClick={this.handleDelete}>
+				{content}
 			</div>
 		);
 	}
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,49 +7,55 @@ class TodoList extends React.Component {
 			list: [],
 			inputValue: ''
 		};
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.HandleBtnClick = this.HandleBtnClick.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
 	}
-	HandleClick() {
+	HandleBtnClick() {
 		this.setState({
 			list: [...this.state.list, this.state.inputValue],
 			inputValue: ''
 		});
 	}
-	handeChange(e) {
+	handleInputChange(e) {
 		this.setState({
 			inputValue: e.target.value
 		});
 	}
 	handleDelete(index) {
-		console.log('handleDelete', index);
 		let list = [...this.state.list];
 		list.splice(index, 1);
 		this.setState({
 			list: list
 		});
 	}
+	getTodoItem(){
+		return (
+			this.state.list.map((item, index) => {
+				return (
+					<TodoItem
+						key={index}
+						content={item}
+						index={index}
+						delete={this.handleDelete}
+					/>
+				);
+			})
+		)
+	}
 	render() {
 		return (
-			<div className="App">
+			<React.Fragment>
 				<div>
 					<input
+						className='blue-input'
 						value={this.state.inputValue}
-						onChange={this.handeChange.bind(this)}
+						onChange={this.handleInputChange}
 					/>
-					<button onClick={this.HandleClick.bind(this)}>add</button>
+					<button style={{'background':'red'}} onClick={this.HandleBtnClick}>add</button>
 				</div>
-				<ul>
-					{this.state.list.map((item, index) => {
-						return (
-							<TodoItem
-								key={index}
-								content={item}
-								index={index}
-								delete={this.handleDelete.bind(this)}
-							/>
-						);
-					})}
-				</ul>
-			</div>
+				<ul>{this.getTodoItem()}</ul>
+			</React.Fragment>
 		);
 	}
 }
